d90f64fa38aad79d73a8dfa65e60ee9a9c424fda
CoolboY61 <3047016035@qq.com>
2022-05-04 19:40:56 +0800

    版本类型

---
+++ src/main/java/com/liu/pdu/type/VersionType.java
@@ -0,0 +1,18 @@
+package com.liu.pdu.type;
+
+/**
+ * SNMP版本类型
+ *
+ * @author : LiuYi
+ * @version :
+ * @date : 2022/5/4 17:16
+ */
+public class VersionType {
+    /**
+     * "version-1 (0)", "version-2 (1)", "version-3 (2)"
+     * 3个版本
+     */
+    public static final String[] VERSION = {
+            "version-1 (0)", "version-2 (1)", "version-3 (2)"
+    };
+}
