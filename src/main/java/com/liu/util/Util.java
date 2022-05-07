c11f64384097adcd66bb73736c398bd6eb6a872d
CoolboY61 <3047016035@qq.com>
2022-05-08 00:30:40 +0800

    工具

--- src/main/java/com/liu/util/Util.java
+++ src/main/java/com/liu/util/Util.java
@@ -1,9 +1,6 @@
 package com.liu.util;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.Charset;
@@ -23,19 +20,6 @@ public class Util {
             "0", "1", "2", "3", "4", "5", "6", "7",
             "8", "9", "a", "b", "c", "d", "e", "f"
     };
-
-    /**
-     * UDP Socket
-     */
-    public static DatagramSocket socket;
-
-    static {
-        try {
-            socket = new DatagramSocket(1000);
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
      * 拼接两个字节数组
@@ -186,33 +170,4 @@ public class Util {
         return String.valueOf(r);
     }
 
-    /**
-     * 使用UDP协议发送SNMP请求，同时接收SNMP响应
-     *
-     * @param snmpData1 需发送的snmp数据
-     * @param iP        目的IP地址
-     * @return 返回接收到的响应
-     */
-    public static byte[] snmpServe(byte[] snmpData1, String iP) {
-        byte[] snmpData2 = new byte[0];
-        byte[] snmpDataTemp;
-        try {
-            int port = 161;
-            InetAddress address = InetAddress.getByName(iP);
-            DatagramPacket packet = new DatagramPacket(snmpData1, snmpData1.length, address, port);
-            socket.setReuseAddress(true);
-            socket.send(packet);
-            byte[] bytes = new byte[1472];
-            DatagramPacket dp = new DatagramPacket(bytes, bytes.length);
-            socket.setSoTimeout(1000);
-            socket.receive(dp);
-            snmpData2 = new byte[dp.getLength()];
-            snmpDataTemp = dp.getData();
-            System.arraycopy(snmpDataTemp, 0, snmpData2, 0, dp.getLength());
-        } catch (Exception e) {
-            System.out.println("\n等待响应超时！！！\n");
-            e.printStackTrace();
-        }
-        return snmpData2;
-    }
 }
